b462c58fc538085fd960d8214aea9672755a0661
Kensei-Sama <52254166+Kensei-Sama@users.noreply.github.com>
2023-01-16 14:05:19 +0100

    update
    before action bar

--- app/src/main/java/com/example/lightweight/MainActivity.java
+++ app/src/main/java/com/example/lightweight/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lightweight;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -10,6 +12,9 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     TextView welcome_text;
+    LinearLayout chest_day;
+    LinearLayout back_biceps_day;
+    LinearLayout shoulder_triceps_day;
 
     int maxRandWelcomeText = 4;
     int minRandWelcomeText = 1;
@@ -20,8 +25,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         welcome_text = findViewById(R.id.welcome_text);
+        chest_day = findViewById(R.id.chest_day);
+        back_biceps_day = findViewById(R.id.back_biceps_day);
+        chest_day = findViewById(R.id.shoulder_triceps_day);
 
         getRandomWelcomeText();
+
+        chest_day.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
 
@@ -32,13 +47,13 @@ public class MainActivity extends AppCompatActivity {
         int welcomeTextRandNum = new Random().nextInt(maxRandWelcomeText - minRandWelcomeText + 1) + minRandWelcomeText;
 
         if (welcomeTextRandNum == 1) {
-            welcome_text.setText("Hello King!");
+            welcome_text.setText("Hello King! 👑");
         }
         if (welcomeTextRandNum == 2) {
             welcome_text.setText("Yeah Buddy!");
         }
         if (welcomeTextRandNum == 3) {
-            welcome_text.setText("Lightweight!!!");
+            welcome_text.setText("Lightweight, Babe!!!");
         }
         if (welcomeTextRandNum == 4) {
             welcome_text.setText("Nice to see you again, Chad.");
